487deb8b94061505d47a87bef092804274891445
lyjuser <103523016+lyjuser@users.noreply.github.com>
2025-05-07 11:25:37 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,8 @@
 from models.transformer_model import ViTB16, SwinB, BeiTB, DiNATB, FocalNetB, ConvNeXtTiny
+from .build import build_model
+from .build import build_model_FAG
+from .build import build_model_FAG_latent
+from .build import build_model_mask
+from .build import build_model_mask_eval
+from .transformer_model import ViTB16, SwinB, BeiTB, DiNATB
+from .cnn_model import ConvNeXtTiny, FocalNetB
